36edd11d8ad7480e1156eb03190b392a60eb4772
Test <test@localhost.localdomain>
2017-12-27 16:30:09 +0530

    package changes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const publicpath = path.join(__dirname,'../public');
-
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicpath));
-app.listen(3000);
+app.listen(port);
